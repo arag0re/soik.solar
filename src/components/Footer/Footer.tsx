9a86dcd39865f1da9adc4e32bd6ac870ac57624b
arag0re <as@sternburg.beer>
2026-05-21 21:19:20 +0200

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,6 @@
-import { useEffect, useState } from 'react'
 import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 
-export default function Footer({ heroHeight = 0 }) {
-   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-   const [scrolled, setScrolled] = useState(false)
-
-   useEffect(() => {
-      const handleScroll = () => {
-         setScrolled(window.scrollY > heroHeight)
-      }
-
-      window.addEventListener('scroll', handleScroll)
-      return () => window.removeEventListener('scroll', handleScroll)
-   }, [heroHeight])
-
+export default function Footer() {
    const socialClass =
       'w-11 h-11 rounded-xl border border-white/10 bg-white/5 flex items-center justify-center text-white/70 hover:text-white hover:bg-white/10 hover:border-white/20 transition-all duration-300'
 
